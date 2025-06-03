3f98dacaf4b98e91899630aef2992201d861dc1c
Janover Saldaña <u20201b510@upc.edu.pe>
2025-06-02 19:55:15 -0500

    feat: Added a base page layout component for view content.

---
+++ src/shared/components/base-page-layout.component.vue
@@ -0,0 +1,33 @@
+<script>
+
+export default {
+  name: "base-page-layout",
+  components: {},
+
+  created() {
+    console.log("Base Page Layout created");
+  }
+};
+
+</script>
+<template>
+  <div class="base-page-layout flex flex-column h-full w-full overflow-hidden">
+
+
+    <div class="layout-header p-3 border-bottom-1 border-300">
+      <slot name="header" />
+    </div>
+
+
+    <div class="layout-body flex-1 overflow-hidden flex flex-column">
+      <slot />
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+.base-page-layout {
+  height: 100%;
+}
+</style>
