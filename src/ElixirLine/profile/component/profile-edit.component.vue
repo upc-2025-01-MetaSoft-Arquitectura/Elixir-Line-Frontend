614a0f02d7d3f98a78cc12d998fa2c012203b5f2
Janover Saldaña <u20201b510@upc.edu.pe>
2025-06-04 14:38:14 -0500

    feat(profile-management): Added profile entity, services and edit component.

---
+++ src/ElixirLine/profile/component/profile-edit.component.vue
@@ -0,0 +1,32 @@
+<script>
+
+
+export default {
+  name: 'ProfileEditComponent',
+
+
+  data() {
+    return {
+      // Define your data properties here
+    };
+  },
+
+
+  methods: {
+    // Define your methods here
+  },
+
+
+  created() {
+  }
+};
+
+</script>
+
+<template>
+
+</template>
+
+<style>
+
+</style>
