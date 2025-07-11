8fceaf3a5d5566f23c0ddb4669055546d312533f
MiguelCarpioC <134799325+LordSack@users.noreply.github.com>
2025-07-10 19:26:32 -0500

    feat: eliminate not founded image

--- src/public/pages/page-not-found.component.vue
+++ src/public/pages/page-not-found.component.vue
@@ -7,9 +7,6 @@ export default {
 <template>
 
   <div>
-    <div>
-      <img src="../../assets/img/page-not-found.jpg" width="400"/>
-    </div>
 
     <div>
       <pv-button>Go to Home</pv-button>
